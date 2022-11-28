042bc75f6e245b85d514c7b4eff0410cdf93b305
Schogi <111045600+Schogi@users.noreply.github.com>
2022-11-28 13:06:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,112 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
-namespace Finanzassisten
+namespace fgj
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            bool Vallah = false;
-            bool DateiAverwendbar= false;
-            bool DateiAVerstanden = false;
-            bool? DateiAVorhanden = null;
-            bool? Fertig = null;
-            var Einnahmen = @"" + Properties.Settings.Default.Einnahmen;
-            var Ausgaben =@""+ Properties.Settings.Default.Ausgaben;
-            List<int> EinnahmenListe = new List<int>();
-            if (Einnahmen == "")
-            {
-                Console.WriteLine("Haben sie schon eine Datei mit ihren Einnahmen ? [J/N]");
-                while (DateiAVorhanden == null)
-                {
-                    char key = Console.ReadKey().KeyChar;
-                    switch (key)
-                    {
-                        case 'j':
-                            DateiAVorhanden = true;
-                            break;
-                        case 'n':
-                            DateiAVorhanden = false;
-                            break;
-                        case 'J':
-                            DateiAVorhanden = true;
-                            break;
-                        case 'N':
-                            DateiAVorhanden= false;
-                            break;
-                            
-                        default:
-                            Console.WriteLine("invalid input");
-                            break;
-                    }
-                    
-                }
-                while (!DateiAverwendbar)
-                {
-                    if (DateiAVorhanden == true)
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine(@"Geben Sie den Dateipfad ein. Beispiel ---> C:\Benutzer\jfrey\documents\Hallo\Test.txt");
-                        while (!Vallah)
-                        {
-                            try
-                            {
-                                Einnahmen = Convert.ToString(Console.ReadLine());
-                                Properties.Settings.Default.Einnahmen = @"";
-                                Vallah = true;
-                            }
-                            catch
-                            {
-                                Console.WriteLine("invalid File path");
-                            }
-                            DateiAverwendbar = true;
-                        }
-                    }
-                    else if (DateiAVorhanden == false)
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine("Erstellen Sie eine txt. Datei und halten sie den Dateipfad bereit.");
-                        Console.WriteLine("Wenn Sie das erledigt haben, drücken Sie die Taste J");
-                        while (Fertig == null)
-                        {
-                            char key2 = Console.ReadKey().KeyChar;
-                            switch (key2)
-                            {
-                                case 'j':
-                                    Fertig = true;
-                                    DateiAVorhanden = true;
-                                    break;
-                                case 'n':
-                                    Fertig = false;
-                                    DateiAVorhanden = false;
-                                    break;
-                                case 'J':
-                                    Fertig = true;
-                                    DateiAVorhanden = true;
-                                    break;
-                                case 'N':
-                                    Fertig = false;
-                                    DateiAVorhanden = false;
-                                    break;
+            string Dateipfad1 = @"C:\Users\haldi\OneDrive\Desktop\Finnannzassisstent.txt";
+            string Dateipfad2 = @"C:\Users\haldi\OneDrive\Desktop\Finnannzassisstent2.txt";
 
-                                default:
-                                    Console.WriteLine("invalid input");
-                                    break;
-                            }
-                            
-                        }
+            string Datei1 = File.ReadAllText(Dateipfad1);
+            string Datei2 = File.ReadAllText(Dateipfad2);
+
+            string[] Linien1 = Datei1.Split("\r\n");
+            string[] Linien2 = Datei2.Split("\r\n");
+
+            int Zeilennummer1 = Linien1.Length;
+            int Zeilennummer2 = Linien2.Length;
+
+            int Ausgaben = 0;
+            int Einnahmen = 0;
+
+            for (int i = 0; i <= Zeilennummer1-1; i++)
+            {
+                Einnahmen = Einnahmen + Convert.ToInt32(Linien1[i]);
+
+            }
+            //Console.WriteLine(Einnahmen);
+
+            for (int j = 0; j <= Zeilennummer2 - 1; j++)
+            {
+                Ausgaben = Ausgaben + Convert.ToInt32(Linien2[j]);
+
+            }
+            //Console.WriteLine(Ausgaben);
+
+            int Verdienst = Einnahmen - Ausgaben;
+            int Sparziel = 0;
+            bool TryCatch = false;
+
+            if (Verdienst<0)
+            {
+                Console.WriteLine("Sie machen " + Verdienst + " Franken minus pro Monat");
+            }
+            else 
+            {
+                Console.WriteLine("Sie machen " + Verdienst + " Franken plus pro Monat");
+            }
+
+
+
+            bool B = false;
+            do
+            {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Wie viel Geld wollen sie sparen?");
+                        Sparziel = Convert.ToInt32(Console.ReadLine());
+                        TryCatch = true;
                     }
+                    catch
+                    {
+                        Console.WriteLine("Ungültige Eingabe");
+                        TryCatch = false;
+                    }
+                } while (TryCatch == false);
+
+                if (Verdienst < 0)
+                {
+                    Console.WriteLine("Da Sie monatlich kein Geld verdienen macht es keinen Sinn zu Spaaren");
+
                 }
-                Console.ReadLine();
-                Console.WriteLine(Einnahmen);
-                Console.ReadLine();
-               
-            } 
+
+                int Sparzeit = Sparziel / Verdienst;
+                int Jahre = 0;
+                int Gschanmargo69 = Sparzeit;
+                if (Sparzeit > 48)
+                {
+                    Jahre = Sparzeit / 12;
+                }
+
+                if (Jahre > 25)
+                {
+                    Console.WriteLine("Sie wollen " + Jahre + " Jahre sparen was ziemlich lange ist, vielleicht sollten Sie sich ein realistischeres Ziel setzen.");
+                    B = true;
+                }
+                else if (Gschanmargo69 > 48)
+                {
+                    Console.WriteLine("Sie werden " + Jahre + " Jahre brauchen um das Ziel zu erreichen");
+                    B=false;
+                }
+                else
+                {
+                    Console.WriteLine("Sie werden " + Gschanmargo69+1 + " Monate brauchen um das Ziel zu erreichen");
+                    B=false;
+                }
+            } while (B == true);
         }
     }
 }
